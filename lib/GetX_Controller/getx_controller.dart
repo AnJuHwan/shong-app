4121f900adc84fe2828e10c405ae5b3b007dc163
AnJuHwan <wnghks0531@gmail.com>
2021-09-21 00:41:03 +0900

    .

--- lib/GetX_Controller/getx_controller.dart
+++ lib/GetX_Controller/getx_controller.dart
@@ -3,19 +3,7 @@ import 'package:get/get.dart';
 class Controller extends GetxController {
   RxList title = [].obs;
 
-  void addTitle(title) {
-    title.add(title);
-  }
-
   RxList post = [].obs;
-
-  void addPost(post) {
-    post.add(post);
-  }
-
-  void addPostingImage(iamge) {
-    postingImage.add(iamge);
-  }
 
   RxList postingImage = [].obs;
 }
